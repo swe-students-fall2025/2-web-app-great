566a41ecddb128179eb6642a80cb0cf06bfdcd72
serena0615 <sw5695@nyu.edu>
2025-10-20 12:46:07 -0400

    Revert "Merge LoopU"

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-import os
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/loopu")
+    # 用于 session / flash 消息的密钥
+    SECRET_KEY = "dev-secret-change-me"
+
+    # 本地 MongoDB
+    MONGO_URI = "mongodb://localhost:27017/loopu"
+
+    # 如果要改成 Atlas，就换下面这个：
+    # MONGO_URI = "mongodb+srv://<username>:<password>@<cluster>.mongodb.net/loopu?retryWrites=true&w=majority"
--- loopu/__init__.py
+++ loopu/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from .db import init_db
-from .routes import bp as main_bp
-from .auth import bp as auth_bp, login_manager
-def create_app(config_obj):
+
+def create_app(config_obj) -> Flask:
     app = Flask(__name__, static_folder="../static", template_folder="../templates")
     app.config.from_object(config_obj)
+
     init_db(app)
-    login_manager.init_app(app)
+
+    from .routes import bp as main_bp
     app.register_blueprint(main_bp)
-    app.register_blueprint(auth_bp)
+
     return app
